16bd02f3272043f7ee8e4e7327c6ec3e0b396894
latentfreedom <latentfreedom@gmail.com>
2021-12-14 18:53:24 -0500

    update to use slash commands.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const {Client, Intents, MessageEmbed} = require('discord.js');
+const {Constants, Client, Intents, MessageEmbed} = require('discord.js');
 const axios = require('axios');
-const client = new Client({intents : [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]});
 require('dotenv').config();
-
-client.once('ready', () => {
-    console.log('Running...')
-    getGas();
-});
 
 let gasPrices = [];
 let alerts = new Map();
 var fs = require('fs');
+
+const client = new Client({
+    intents : [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+client.on('ready', () => {
+    console.log('Gas Tracker Running...');
+    createCommands();
+    getGas();
+});
 
 const saveAlerts = (alertData) => {
     fs.writeFile("./alerts.json", JSON.stringify(alertData), function(err) {
@@ -29,6 +33,33 @@ const getAlerts = () => {
     });
 }
 
+const createCommands = () => {
+    const Guilds = client.guilds.cache.map(guild => guild.id);
+    // Add commands to all guilds
+    Guilds.forEach(guildId => {
+        const guild = client.guilds.cache.get(guildId);
+        let commands = guild.commands;
+        // gas command
+        commands?.create({
+            name: "gas",
+            description: "replies with current gas prices on etherscan"
+        });
+        // alert command
+        commands?.create({
+            name: "alert",
+            description: "alert user when gas reaches specified amount",
+            options: [
+                {
+                    name: "amount",
+                    description: "gas amount to be alerted for",
+                    required: true,
+                    type: Constants.ApplicationCommandOptionTypes.NUMBER
+                }
+            ]
+        })
+    });
+}
+
 const styleMessage = () => {
     const embed = new MessageEmbed().setTitle('⛽ Current Gas Prices');
     if (gasPrices.result.FastGasPrice > 100) {
@@ -40,10 +71,10 @@ const styleMessage = () => {
     }
 
     embed.addFields({ name: 'Slow 🐢 | >10 minutes', value: `${gasPrices.result.SafeGasPrice} Gwei` },
-        { name: 'Proposed 🙂 | 3 minutes', value: `${gasPrices.result.ProposeGasPrice} Gwei` },
+        { name: 'Proposed 🚶 | 3 minutes', value: `${gasPrices.result.ProposeGasPrice} Gwei` },
         { name: 'Fast ⚡ | 15 seconds', value: `${gasPrices.result.FastGasPrice} Gwei` },
     );
-    return {embeds : [embed]};
+    return [embed];
     
 }
 
@@ -52,10 +83,9 @@ getGas = () => {
     let req = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.ETHERSCAN_PRIV}`;
     axios.get(req).then(res => {
         gasPrices = res.data;
-        client.user.setActivity(`⚡${gasPrices.result.FastGasPrice} 🙂${gasPrices.result.ProposeGasPrice} 🐢${gasPrices.result.SafeGasPrice}`);
+        client.user.setActivity(`⚡${gasPrices.result.FastGasPrice} 🚶${gasPrices.result.ProposeGasPrice} 🐢${gasPrices.result.SafeGasPrice}`);
         checkAlerts();
     })
-    
 }
 
 checkAlerts = () => {
@@ -74,31 +104,38 @@ checkAlerts = () => {
     }
 }
 
-setInterval(getGas, 10 * 5000);
+setInterval(getGas, 10 * 3000);
 
-client.on('messageCreate', message => {
-    const prefix = '/gas';
-    if (message.content.startsWith(prefix)) {
-        let args = message.content.slice(prefix.length+1).trim().split(' ');
-        if(args[0] === '') args = args.splice(0,0);
-        if(args.length === 0) {
-            message.channel.send(styleMessage());
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) { return; }
+    const { commandName, options } = interaction;
+    if (commandName === 'gas') {
+        // Process gas command
+        await interaction.deferReply({
+            ephemeral: true,
+        });
+        interaction.editReply({
+            embeds: styleMessage(),
+        })
+    } else if (commandName === 'alert') {
+        // Process alert command
+        const amount = options.getNumber('amount');
+        const user = interaction.user;
+        const name = user.username;
+        interaction.reply({
+            content: `Thanks, **${name}**. I will send a private message when gas is below **${amount}** Gwei.`,
+            ephemeral: true
+        })
+        // Add alert to Mapping
+        if(!alerts.has(user)) {
+            alerts.set(user, [amount]);
+        } else {
+            let newAlertList = alerts.get(user);
+            newAlertList.push(amount);
+            alerts.set(user, newAlertList);
         }
-        if(args[0] === 'alert' && args.length === 2) {
-            let amount = args[1];
-            let user = message.author;
-            let name = message.member.nickname ? message.member.nickname : message.member.user.username;
-            message.channel.send(`Thanks, ${name}. I will send you a private message when gas price drops below ${amount} gwei.`);
-            if(!alerts.has(user)) {
-                alerts.set(user, [amount]);
-            } else {
-                let newAlertList = alerts.get(user);
-                newAlertList.push(amount);
-                alerts.set(user, newAlertList);
-            }
-            saveAlerts(alerts);
-        }
+        saveAlerts(alerts);
     }
-});
+})
 
 client.login(process.env.DISCORD_TOKEN);
